cf45966cd625df7f7140c90fab3fa2cd0527a8ec
Warren Day (Work) <work@Warrens-MacBook-Pro-3.local>
2019-05-07 14:57:46 +0100

    formatting

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import Form from './Form';
 
 function App() {
-  const [values, setValues] = useState(null)
+  const [values, setValues] = useState(null);
 
   return (
     <div>
       <Form onSubmit={setValues} />
-
       {values && (
         <>
           <div style={{ marginTop: 20 }}>Submitted Values:</div>
           <div>
             {Object.entries(values).map(([key, value]) => (
-              <div>{key}: {value}</div>
+              <div>
+                {key}: {value}
+              </div>
             ))}
           </div>
         </>
--- src/Form/index.js
+++ src/Form/index.js
@@ -2,36 +2,41 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
-const wait = (time) => new Promise(resolve => setTimeout(resolve, time))
+const wait = time => new Promise(resolve => setTimeout(resolve, time));
 
 const validationSchema = yup.object().shape({
   name: yup.string().required(),
-  email: yup.string().email().required(),
+  email: yup
+    .string()
+    .email()
+    .required(),
   age: yup.number().required(),
 });
 
-const Input = (props) => {
-  const { field } = props
+const Input = props => {
+  const { field } = props;
   return (
     <div>
       {field.name}
       <input {...field} />
-      <ErrorMessage name={field.name} />  
+      <ErrorMessage name={field.name} />
     </div>
-  )
-}
+  );
+};
 
 const MyForm = ({ onSubmit }) => {
+  const handleSubmit = async (values, actions) => {
+    await wait(1000);
+    actions.setSubmitting(false);
+    actions.setStatus(new Date().toISOString());
+    onSubmit(values);
+  };
+
   return (
     <Formik
       initialValues={{ name: '', email: '' }}
       validationSchema={validationSchema}
-      onSubmit={async (values, actions) => {
-        await wait(1000);
-        actions.setSubmitting(false);
-        actions.setStatus(new Date().toISOString());
-        onSubmit(values);
-      }}
+      onSubmit={handleSubmit}
       onReset={() => onSubmit(null)}
       render={({ status, isSubmitting }) => {
         return (
@@ -39,12 +44,14 @@ const MyForm = ({ onSubmit }) => {
             <Field name="name" component={Input} />
             <Field type="email" name="email" component={Input} />
             <Field type="number" name="age" component={Input} />
-            <button type="submit" disabled={isSubmitting}>Submit</button>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
             <button type="reset">Reset</button>
             <div>Submit time: {status || 'never'}</div>
             {isSubmitting && <div>Submitting...</div>}
           </Form>
-        )
+        );
       }}
     />
   );
