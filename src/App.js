020b6ffe4e76101a8009002a183a780ca4f44b2a
Warren Day (Work) <work@Warrens-MacBook-Pro-3.local>
2019-05-07 13:47:54 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Form from './Form';
 
 function App() {
+  const [values, setValues] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form onSubmit={setValues} />
+
+      {values && (
+        <>
+          <div style={{ marginTop: 20 }}>Submitted Values:</div>
+          <div>
+            {Object.entries(values).map(([key, value]) => (
+              <div>{key}: {value}</div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
